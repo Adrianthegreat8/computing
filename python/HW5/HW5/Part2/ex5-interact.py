16a8a4b8bec8ba33f96afa5d3195f44aa0df5eb1
Adrianthegreat8 <Adrianthegreat8@gmail.com>
2024-02-23 15:24:54 -0500

    fixed some spacing

--- python/HW5/HW5/Part2/ex5-interact.py
+++ python/HW5/HW5/Part2/ex5-interact.py
@@ -1,26 +1,24 @@
-#aibanez1:02/22/2024:ex5-interact.py
+# aibanez1:02/22/2024:ex5-interact.py
 
-Laz=['***','temperature anomaly AZ',1.60,1.48,1.78,1.80,1.88,1.90,1.50]
+Laz = ['***', 'temperature anomaly AZ', 1.60, 1.48, 1.78, 1.80, 1.88, 1.90, 1.50]
 
-Tstate=('A~r`i^z@o>n#a', 'Nevada','Colorado','Colorado')
+Tstate = ('A~r`i^z@o>n#a', 'Nevada', 'Colorado', 'Colorado')
 
-D={1:'This', 2:'is', 3:'an interactive', 4: 'script.'}
+D = {1: 'This', 2: 'is', 3: 'an interactive', 4: 'script.'}
 
-T=("Hello,","no sense", "this is the end.")     
+T = ("Hello,", "no sense", "this is the end.")
 
-Dprot={'prot1':{'code':'GATTACA'},'prot2':['ACATTA',34,56,78],'prot3':('DNA','RNA')}
+Dprot = {'prot1': {'code': 'GATTACA'}, 'prot2': ['ACATTA', 34, 56, 78], 'prot3': ('DNA', 'RNA')}
 
-s='Hello Schni'
+s = 'Hello Schni'
 print(s[:-5])
 
-#Q1
-Laz_clean=Laz[2:]
+# Q1
+Laz_clean = Laz[2:]
 print(Laz_clean)
 
-#Q2
-Tstate_1=Tstate[0]
+# Q2
+Tstate_1 = Tstate[0]
 print(Tstate_1[0:14:2])
 
-#Q3
-
-
+# Q3
