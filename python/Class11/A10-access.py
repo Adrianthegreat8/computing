79ae1eab0fd2f22615089c171d64d97a19750831
Adrianthegreat8 <Adrianthegreat8@gmail.com>
2024-02-27 11:31:15 -0500

    completed A10-access.py

---
+++ python/Class11/A10-access.py
@@ -0,0 +1,35 @@
+#aibanez1:02/27/2024:A10-access.py
+
+#QA
+s='Python Programming!'
+L=[1,2,3,4,5,6,[7,8,9]]
+T=("Hello","Ciao","Hola")
+D={'pop':3, 'jazz':10, 'blues':6}
+
+#QB
+print(s[-1])
+
+#QC
+print(s[0:6])
+
+#QD
+print(s[::-1])
+
+#QE
+print(s[2:11:2])
+
+#QF
+L1=(L[0:4])
+print(L1)
+
+#QG
+print(L[-1][-1])
+
+#QH
+print(T[1])
+
+#QI
+print(D['jazz'])
+
+#QJ
+print(s, D)
