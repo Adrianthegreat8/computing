710531e8f9c0ca059616c92451b25623df67ee99
Adrianthegreat8 <Adrianthegreat8@gmail.com>
2024-02-29 13:07:54 -0500

    Finish in class 11 for class 12

--- python/Class12/A11-dict.py
+++ python/Class12/A11-dict.py
@@ -4,7 +4,30 @@
 D={"orange":3, "blue":4, "black":5}
 
 #QB
-D1=D
+D1=D.copy()
 
 #QC
 print(len(D))
+
+#QD
+D["purple"]=4
+print(D)
+
+#QE
+D.update({"red":6})
+print(D)
+
+#QF
+t=D.popitem()
+print(D)
+
+#QG
+v=D.pop("orange")
+print(D)
+
+#QH
+del D["blue"]
+print(D)
+
+#QI
+print(D1)
